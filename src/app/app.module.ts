cb056df809ebffc46afd23fd493f66256657e654
madymadesc <madymadesc@gmail.com>
2023-01-12 12:32:38 +0000

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component'
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
-    FaceSnapListComponent
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
